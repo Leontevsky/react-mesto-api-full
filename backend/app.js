a2d28506c76bf735cf323c19de56456741a404a2
leontevsky <alexander.leontevsky@gmail.com>
2022-02-01 11:26:01 +0300

    backend cors

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@
 const express = require('express');
 // подключение mongoose
 const mongoose = require('mongoose');
+const helmet = require("helmet");
+const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 // подключение router
@@ -12,6 +14,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const handleError = require("./middlewares/handleError");
 const {
   validationLogin,
   validationCreateUser,
@@ -26,27 +29,46 @@ const app = express();
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+const options = {
+  origin: [
+    "http://leontevsky.nomoredomains.work",
+    "https://leontevsky.nomoredomains.work",
+    "http://leontevskyback.nomoredomains.work",
+    "https://leontevskyback.nomoredomains.work",
+    "http://localhost:3000",
+    "https://localhost:3000",
+  ],
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ["Content-Type", "origin", "Authorization", "Accept"],
+  credentials: true,
+};
+
+app.use("*", cors(options));
+app.use(helmet());
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', validationLogin, login);
 app.post('/signup', validationCreateUser, createUser);
 
 app.use('/', auth, userRouter);
 app.use('/', auth, cardRouter);
-app.use(() => {
+app.use('*', auth, () => {
   throw new NotFound('Роутер не найден');
 });
 app.use(errorLogger);
 app.use(errors());
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-  next();
-});
+app.use(handleError);
 
 // Прослушиваем подключение на порту 3000
 app.listen(PORT, () => {
